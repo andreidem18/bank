2366536244c1b93ace587ff00602f722148bd605
Andrés Mendoza <andres.david.mm@hotmail.com>
2021-04-27 08:54:48 -0500

    changing to heroku database

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    use_env_variable: 'DB_URL_AWS',
+    use_env_variable: 'DATABASE_URL',
     dialectOptions: {
       ssl:{
         require: true,
